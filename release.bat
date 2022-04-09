b665f045809522f50e9b32790f75b52ea82124c1
Jannis Berndt <berndtjannis@gmail.com>
2022-04-09 14:57:54 +0200

    Adjust release script and make changelog cider compatible

--- release.bat
+++ release.bat
@@ -9,6 +9,7 @@ if "%explicit_version%" == "true" (
 ) else (
     call cider bump %1 >NUL
 )
+call flutter pub get
 
 FOR /F "tokens=* USEBACKQ" %%F IN (`cider version`) DO (
     SET new_ver=%%F
@@ -18,7 +19,7 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`cider version`) DO (
 @echo Changes to release:
 call cider describe
 call cider release
-call git commit -am "Version v%new_ver%"
+call git commit pubspec.yaml CHANGELOG.md example/pubspec.lock -m "Version v%new_ver%"
 call git tag -a v%new_ver% -m "Release v%new_ver%"
 call git push --tags
 exit /B 0
