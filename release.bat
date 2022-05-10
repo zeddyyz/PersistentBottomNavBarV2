cf7891cc668257321f43fcd77283750a35e0e7a2
Jannis Berndt <berndtjannis@gmail.com>
2022-05-10 23:00:19 +0200

    Update tag message

--- release.bat
+++ release.bat
@@ -19,8 +19,8 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`cider version`) DO (
 @echo Changes to release:
 call cider describe
 call cider release
-call git commit pubspec.yaml CHANGELOG.md example/pubspec.lock -m "Version v%new_ver%"
-call git tag -a v%new_ver% -m "Release v%new_ver%"
+call git commit pubspec.yaml CHANGELOG.md example/pubspec.lock -m "Version %new_ver%"
+call git tag -a v%new_ver% -m "Release Version %new_ver%"
 call git push --tags
 exit /B 0
 
