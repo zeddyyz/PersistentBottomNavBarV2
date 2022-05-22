07a003660f4d8d81628ba70e71ca89dd3f901964
Jannis Berndt <berndtjannis@gmail.com>
2022-05-22 13:50:06 +0200

    Add missing tests for custom persistent navbar

--- test/persistent-tab-view.widget_test.dart
+++ test/persistent-tab-view.widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:persistent_bottom_nav_bar_v2/persistent-tab-view.dart';
@@ -62,8 +63,7 @@ void main() {
       expect(find.byType(PersistentBottomNavBar).hitTestable(), findsOneWidget);
     });
 
-    testWidgets('shows all tabs and the correct screen for it',
-        (WidgetTester tester) async {
+    testWidgets('can switch through tabs', (WidgetTester tester) async {
       await tester.pumpWidget(
         wrapTabView(
           (context) => PersistentTabView(
@@ -93,27 +93,6 @@ void main() {
       expect(find.text('Screen1'), findsOneWidget);
       expect(find.text('Screen2'), findsNothing);
       expect(find.text('Screen3'), findsNothing);
-    });
-
-    testWidgets('switches screens when tapping on items',
-        (WidgetTester tester) async {
-      await tester.pumpWidget(
-        wrapTabView(
-          (context) => PersistentTabView(
-            context,
-            screens: [1, 2, 3].map((id) => defaultScreen(id)).toList(),
-            items: items,
-            navBarStyle: NavBarStyle.style3,
-          ),
-        ),
-      );
-
-      expect(find.text('Screen1'), findsOneWidget);
-      expect(find.text('Screen2'), findsNothing);
-      await tester.tap(find.text("Item2"));
-      await tester.pumpAndSettle();
-      expect(find.text('Screen1'), findsNothing);
-      expect(find.text('Screen2'), findsOneWidget);
     });
 
     testWidgets('hides the navbar when hideNavBar is true',
@@ -468,6 +447,35 @@ void main() {
       });
     });
 
+    testWidgets("navBarPadding adds padding inside navBar",
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        wrapTabView(
+          (context) => PersistentTabView(
+            context,
+            screens: [1, 2, 3].map((id) => defaultScreen(id)).toList(),
+            items: items,
+            navBarStyle: NavBarStyle.style3,
+            padding: NavBarPadding.all(0),
+          ),
+        ),
+      );
+      double originalIconSize = tester.getSize(find.byType(Icon).first).height;
+
+      await tester.pumpWidget(
+        wrapTabView(
+          (context) => PersistentTabView(
+            context,
+            screens: [1, 2, 3].map((id) => defaultScreen(id)).toList(),
+            items: items,
+            navBarStyle: NavBarStyle.style3,
+            padding: NavBarPadding.all(4),
+          ),
+        ),
+      );
+      expect(tester.getSize(find.byType(Icon).first).height,
+          equals(originalIconSize - 4 * 2));
+    });
     testWidgets("navBarPadding does not make navbar bigger",
         (WidgetTester tester) async {
       await tester.pumpWidget(
@@ -896,6 +904,306 @@ void main() {
               Offset(800, 600 - 56 - margin.vertical),
           equals(margin.topRight));
     });
+    testWidgets("backgroundColor is shown behind the navBar",
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        wrapTabView(
+          (context) => CustomView(
+            backgroundColor: Colors.amber,
+          ),
+        ),
+      );
+
+      expect(
+          ((tester.firstWidget(
+            (find.descendant(
+              of: find.byType(PersistentBottomNavBar),
+              matching: find.byType(Container),
+            )),
+          ) as Container)
+              .color as Color),
+          Colors.amber);
+    });
+    group("should handle Android back button press and thus", () {
+      testWidgets("switches to first tab on back button press",
+          (WidgetTester tester) async {
+        await tester.pumpWidget(
+          wrapTabView(
+            (context) => CustomView(
+              handleAndroidBackButtonPress: true,
+            ),
+          ),
+        );
+
+        expect(find.text('Screen1'), findsOneWidget);
+        expect(find.text('Screen2'), findsNothing);
+        await tester.tap(find.text("Item2"));
+        await tester.pumpAndSettle();
+        expect(find.text('Screen1'), findsNothing);
+        expect(find.text('Screen2'), findsOneWidget);
+
+        await tapAndroidBackButton(tester);
+
+        expect(find.text('Screen1'), findsOneWidget);
+        expect(find.text('Screen2'), findsNothing);
+      });
+
+      testWidgets("pops one screen on back button press",
+          (WidgetTester tester) async {
+        await tester.pumpWidget(
+          wrapTabView(
+            (context) => CustomView(
+              handleAndroidBackButtonPress: true,
+            ),
+          ),
+        );
+
+        await tester.tap(find.byType(ElevatedButton));
+        await tester.pumpAndSettle();
+        expect(find.text("Screen1"), findsNothing);
+        expect(find.text("Screen11"), findsOneWidget);
+
+        await tapAndroidBackButton(tester);
+
+        expect(find.text('Screen1'), findsOneWidget);
+        expect(find.text('Screen11'), findsNothing);
+      });
+    });
+
+    group("should not handle Android back button press and thus", () {
+      testWidgets("does not switch to first tab on back button press",
+          (WidgetTester tester) async {
+        await tester.pumpWidget(
+          wrapTabView(
+            (context) => CustomView(
+              handleAndroidBackButtonPress: false,
+            ),
+          ),
+        );
+
+        expect(find.text('Screen1'), findsOneWidget);
+        expect(find.text('Screen2'), findsNothing);
+        await tester.tap(find.text("Item2"));
+        await tester.pumpAndSettle();
+        expect(find.text('Screen1'), findsNothing);
+        expect(find.text('Screen2'), findsOneWidget);
+
+        await tapAndroidBackButton(tester);
+
+        expect(find.text('Screen1'), findsNothing);
+        expect(find.text('Screen2'), findsOneWidget);
+      });
+
+      testWidgets("pops no screen on back button press",
+          (WidgetTester tester) async {
+        await tester.pumpWidget(
+          wrapTabView(
+            (context) => CustomView(
+              handleAndroidBackButtonPress: false,
+            ),
+          ),
+        );
+
+        await tester.tap(find.byType(ElevatedButton));
+        await tester.pumpAndSettle();
+        expect(find.text("Screen1"), findsNothing);
+        expect(find.text("Screen11"), findsOneWidget);
+
+        await tapAndroidBackButton(tester);
+
+        expect(find.text("Screen1"), findsNothing);
+        expect(find.text("Screen11"), findsOneWidget);
+      });
+    });
+
+    testWidgets(
+        'resizes screens to avoid bottom inset according to resizeToAvoidBottomInset',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        wrapTabView(
+          (context) => MediaQuery(
+            data: MediaQueryData(
+              viewInsets: const EdgeInsets.only(
+                  bottom: 100), // Simulate an open keyboard
+            ),
+            child: Builder(builder: (context) {
+              return CustomView(
+                resizeToAvoidBottomInset: true,
+              );
+            }),
+          ),
+        ),
+      );
+
+      expect(tester.getSize(find.byType(PersistentTabScaffold)).height,
+          equals(500));
+
+      await tester.pumpWidget(
+        wrapTabView(
+          (context) => MediaQuery(
+            data: MediaQueryData(
+              viewInsets: const EdgeInsets.only(
+                  bottom: 100), // Simulate an open keyboard
+            ),
+            child: Builder(builder: (context) {
+              return CustomView(
+                resizeToAvoidBottomInset: false,
+              );
+            }),
+          ),
+        ),
+      );
+
+      expect(tester.getSize(find.byType(PersistentTabScaffold)).height,
+          equals(600));
+    });
+
+    testWidgets('resizes screens by bottomScreenMargin',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        wrapTabView(
+          (context) => CustomView(
+            bottomScreenMargin: kBottomNavigationBarHeight,
+          ),
+        ),
+      );
+
+      expect(tester.getSize(find.byKey(Key("TabSwitchingView"))).height,
+          equals(600 - kBottomNavigationBarHeight));
+    });
+    testWidgets('pops screens when tapping same tab if specified to do so',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        wrapTabView(
+          (context) => CustomView(
+            popAllScreensOnTapOfSelectedTab: true,
+          ),
+        ),
+      );
+
+      await tester.tap(find.byType(ElevatedButton));
+      await tester.pumpAndSettle();
+      expect(find.text("Screen1"), findsNothing);
+      expect(find.text("Screen11"), findsOneWidget);
+      await tester.tap(find.text("Item1"));
+      await tester.pumpAndSettle();
+      expect(find.text("Screen1"), findsOneWidget);
+      expect(find.text("Screen11"), findsNothing);
+
+      await tester.pumpWidget(
+        wrapTabView(
+          (context) => CustomView(
+            popAllScreensOnTapOfSelectedTab: false,
+          ),
+        ),
+      );
+
+      await tester.tap(find.byType(ElevatedButton));
+      await tester.pumpAndSettle();
+      expect(find.text("Screen1"), findsNothing);
+      expect(find.text("Screen11"), findsOneWidget);
+      await tester.tap(find.text("Item1"));
+      await tester.pumpAndSettle();
+      expect(find.text("Screen1"), findsNothing);
+      expect(find.text("Screen11"), findsOneWidget);
+    });
+    testWidgets('pops all screens when tapping same tab',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        wrapTabView(
+          (context) => CustomView(
+            popAllScreensOnTapOfSelectedTab: true,
+          ),
+        ),
+      );
+
+      await tester.tap(find.byType(ElevatedButton));
+      await tester.pumpAndSettle();
+      expect(find.text("Screen1"), findsNothing);
+      expect(find.text("Screen11"), findsOneWidget);
+      await tester.tap(find.byType(ElevatedButton));
+      await tester.pumpAndSettle();
+      expect(find.text("Screen1"), findsNothing);
+      expect(find.text("Screen11"), findsNothing);
+      expect(find.text("Screen111"), findsOneWidget);
+      await tester.tap(find.text("Item1"));
+      await tester.pumpAndSettle();
+      expect(find.text("Screen1"), findsOneWidget);
+      expect(find.text("Screen11"), findsNothing);
+      expect(find.text("Screen111"), findsNothing);
+    });
+
+    testWidgets('persists screens while switching if stateManagement turned on',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        wrapTabView(
+          (context) => CustomView(
+            stateManagement: true,
+          ),
+        ),
+      );
+
+      await tester.tap(find.byType(ElevatedButton));
+      await tester.pumpAndSettle();
+      expect(find.text("Screen1"), findsNothing);
+      expect(find.text("Screen11"), findsOneWidget);
+      await tester.tap(find.text("Item2"));
+      await tester.pumpAndSettle();
+      expect(find.text("Screen2"), findsOneWidget);
+      expect(find.text("Screen11"), findsNothing);
+      await tester.tap(find.text("Item1"));
+      await tester.pumpAndSettle();
+      expect(find.text("Screen1"), findsNothing);
+      expect(find.text("Screen11"), findsOneWidget);
+    });
+
+    testWidgets('trashes screens while switching if stateManagement turned off',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        wrapTabView(
+          (context) => PersistentTabView(
+            context,
+            screens: [1, 2, 3].map((id) => screenWithSubPages(id)).toList(),
+            items: items,
+            navBarStyle: NavBarStyle.style3,
+            stateManagement: false,
+            screenTransitionAnimation: ScreenTransitionAnimation(
+              animateTabTransition: true,
+              curve: Curves.ease,
+              duration: Duration(milliseconds: 200),
+            ),
+          ),
+        ),
+      );
+
+      await tester.tap(find.byType(ElevatedButton));
+      await tester.pumpAndSettle();
+      expect(find.text("Screen1"), findsNothing);
+      expect(find.text("Screen11"), findsOneWidget);
+      await tester.tap(find.text("Item2"));
+      await tester.pumpAndSettle();
+      expect(find.text("Screen2"), findsOneWidget);
+      expect(find.text("Screen11"), findsNothing);
+      await tester.tap(find.text("Item1"));
+      await tester.pumpAndSettle();
+      expect(find.text("Screen11"), findsNothing);
+      expect(find.text("Screen1"), findsOneWidget);
+    });
+     testWidgets('shows FloatingActionButton if specified',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        wrapTabView(
+          (context) => CustomView(
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {},
+              child: Icon(Icons.add),
+            ),
+          ),
+        ),
+      );
+
+      expect(find.byType(FloatingActionButton).hitTestable(), findsOneWidget);
+    });
   });
 }
 
@@ -904,12 +1212,26 @@ class CustomView extends StatefulWidget {
   final bool hideNavigationBarWhenKeyboardShows;
   final double navBarHeight;
   final EdgeInsets margin;
+  final Color backgroundColor;
+  final bool handleAndroidBackButtonPress;
+  final bool resizeToAvoidBottomInset;
+  final bool popAllScreensOnTapOfSelectedTab;
+  final double? bottomScreenMargin;
+  final bool stateManagement;
+  final Widget? floatingActionButton;
 
   CustomView({
     this.hideNavBar = false,
     this.hideNavigationBarWhenKeyboardShows = true,
     this.navBarHeight = kBottomNavigationBarHeight,
     this.margin = EdgeInsets.zero,
+    this.backgroundColor = CupertinoColors.white,
+    this.handleAndroidBackButtonPress = true,
+    this.resizeToAvoidBottomInset = true,
+    this.bottomScreenMargin,
+    this.popAllScreensOnTapOfSelectedTab = true,
+    this.stateManagement = true,
+    this.floatingActionButton,
   });
 
   @override
@@ -922,7 +1244,7 @@ class _CustomViewState extends State<CustomView> {
     return Material(
       child: PersistentTabView.custom(
         context,
-        screens: [1, 2, 3].map((id) => defaultScreen(id)).toList(),
+        screens: [1, 2, 3].map((id) => screenWithSubPages(id)).toList(),
         items: items,
         itemCount: 3,
         hideNavigationBar: widget.hideNavBar,
@@ -930,6 +1252,13 @@ class _CustomViewState extends State<CustomView> {
             widget.hideNavigationBarWhenKeyboardShows,
         navBarHeight: widget.navBarHeight,
         margin: widget.margin,
+        backgroundColor: widget.backgroundColor,
+        handleAndroidBackButtonPress: widget.handleAndroidBackButtonPress,
+        resizeToAvoidBottomInset: widget.resizeToAvoidBottomInset,
+        bottomScreenMargin: widget.bottomScreenMargin,
+        popAllScreensOnTapOfSelectedTab: widget.popAllScreensOnTapOfSelectedTab,
+        stateManagement: widget.stateManagement,
+        floatingActionButton: widget.floatingActionButton,
         customWidget: (navBarEssentials) => Container(
           key: Key("customNavBar"),
           child: Row(
